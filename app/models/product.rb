8bb27c7ee544685a78e673802975ef71a3470f0b
zhuhaofeng <375633799@qq.com>
2011-07-15 15:14:30 +0800

    The second commit with basical Carts

--- app/models/product.rb
+++ app/models/product.rb
@@ -7,4 +7,17 @@ validates :image_url, :format =>{
 	:message   => 'must be a URL for GIF,JPG or PNG image.'
 }
 default_scope :order => 'title'
+has_many :line_items
+
+before_destroy :ensure_not_referenced_by_any_line_item
+
+# ensure that there are no line items referencing this product
+def ensure_not_referenced_by_any_line_item
+  if line_items.count.zero?
+    return true
+  else
+    errors[:base] << "Line Items present"
+    return false
+  end
+end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,19 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110714061704) do
+ActiveRecord::Schema.define(:version => 20110715055948) do
+
+  create_table "carts", :force => true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "line_items", :force => true do |t|
+    t.integer  "product_id"
+    t.integer  "cart_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "products", :force => true do |t|
     t.string   "title"
